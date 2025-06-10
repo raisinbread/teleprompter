56254652900a46a9705757ac73f4153e07e7f53a
John Anderson <john@channel99.com>
2025-06-10 16:28:03 -0600

    Initial commit.

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        myCustomGlobal: 'readonly',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+); 
---
+++ src/test/index.test.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest';
+
+// Since index.ts does not export anything, we need to dynamically import it
+
+describe('Server startup', () => {
+  it('happens without error', async () => {
+    let error: unknown = null;
+    try {
+      await import('../index');
+    } catch (err) {
+      error = err;
+    }
+    expect(error).toBeNull();
+  });
+});
